ab95346d0f28c46dbf0a356a0b7ff7a5f71eab47
iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>
2023-12-18 15:57:18 -0700

    yay

--- js/xmas.js
+++ js/xmas.js
@@ -33,7 +33,7 @@
 
         //do something later when date is reached
         if (distance < 0) {
-          document.getElementById("headline").innerText = "Marry Christmas!";
+          document.getElementById("headline").innerText = "Merry Christmas!";
           document.getElementById("countdown").style.display = "none";
           document.getElementById("content").style.display = "block";
           clearInterval(x);
